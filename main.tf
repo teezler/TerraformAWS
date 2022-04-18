0067145261b04569df1e551fecbe8f322b01281f
Tomide <lanreola.tomi@gmail.com>
2022-04-18 18:29:29 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
 
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
